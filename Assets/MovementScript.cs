0f9ae543c945d6b2751bc358b99ff311de0e01c8
Tara-PC\Tara <taragada@gmail.com>
2017-01-04 21:19:31 +0100

    Blood God
    Ajout sur github, une fois le projet fini (pas encore l'habitude :) )

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    Rigidbody rig;
+    public int amplitude = 1;
+    private int pop;
+
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody>();
+        pop = Random.Range(0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rig.velocity = new Vector3(0.0f, Mathf.Sin(Time.time+pop) * amplitude, 0.0f);
+	}
+}
